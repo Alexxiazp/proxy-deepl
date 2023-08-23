02991c129e5a47ce3efd715277e13abf7dbd3751
夏志鹏 <46433618+Alexxiazp@users.noreply.github.com>
2023-08-23 15:37:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/", createProxyMiddleware(proxyOptions));
 
 
 // 启动服务器
-const PORT = 8000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
